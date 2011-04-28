65fe596634b219857c9ab56dfa27de734426e03a
Meno Abels <meno.abels@adviser.com>
2011-04-28 20:46:22 +0200

    now its alpha

--- conf-dispatcher/server.js
+++ conf-dispatcher/server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var url = require('url');
+var util = require('util');
 var fs = require('fs');
 
 var CouchClient = require('./couch-client');
@@ -8,15 +9,15 @@ var callStreamie = function(token, fn) {
 	var streamie = http.request({
 					host: 'streamie.org',
 					//host: '172.29.29.222',
-                                        //port: 8080,
-                                        port: 80,
-                                        path: '/user_info',
+					//port: 8080,
+					port: 80,
+					path: '/user_info',
 					method: 'GET',
-                                        headers: {
-						   'Host': 'streamie.org',
-                                 		   'Cookie': 'token='+token         						 
-                                                 }
-                                    }, function(res) { 
+					headers: {
+					  'Host': 'streamie.org',
+				 	  'Cookie': 'token='+token         						 
+				  }
+		}, function(res) { 
 					res.setEncoding('utf8')
 					res.on('data', function(doc) {
 						try {
@@ -27,24 +28,23 @@ var callStreamie = function(token, fn) {
 							console.error('callStreamie:exception:'+e)
 						}
 					}) 
-				    }).end()
+	}).end()
 }
 
 var getMacAddress = function(address, fn) {
   fs.readFile('/proc/net/arp', 'utf8', function(err, data) {
-	if (err) { 
-		console.error('can not read /proc/net/arp:'+err) 
-		return
-	}
-	var lines = data.toString().split("\n")
-
-	for(var i = lines.length-1; i >= 0; --i) {
-		var line = lines[i].split(/\s+/)
-		if (line[0] == address) {
-			return fn(line[3])
+		if (err) { 
+			console.error('can not read /proc/net/arp:'+err) 
+			return
 		}
-	}
-	fn()
+		var lines = data.toString().split("\n")
+		for(var i = lines.length-1; i >= 0; --i) {
+			var line = lines[i].split(/\s+/)
+			if (line[0] == address) {
+				return fn(line[3])
+			}
+		}
+		fn()
   })
 }
 
@@ -53,30 +53,37 @@ var updateStreamie = function(mac, req, ret) {
     if (err) { console.error('couchdb:get:failure:'+err) }
 //console.log('mac:'+mac+":"+JSON.stringify(ret)+":"+JSON.stringify(doc))
 //console.log(req)
+		var client = {
+									 ipv4: req.headers['x-real-ip'] || req.socket.remoteAddress,
+									 hwaddr: mac,
+									 useragent: req.headers['user-agent'],
+									 created_at: new Date()
+								 };
     if (!doc) {
+console.log('NEW-DOC');
       doc = {
              _id: ret.user_id, 
              twitter: ret,
-             clients: [{
-                        ipv4: req.headers['x-real-ip'] || req.socket.remoteAddress,
-                        hwaddr: mac,
-                        useragent: req.headers['user-agent'],
-                        created_at: new Date()
-                       }]
+             clients: [client]
             }
     } else {
+console.log('UPDATE-DOC');
       doc.twitter = ret	
       delete doc.completed
+      var found = false;
       for(var i = doc.clients.length-1; i >= 0; --i) {
-        var clients = doc.clients[i]
-        if (clients.ipv4 == req.headers['x-real-ip'] || req.socket.remoteAddress) {
-          clients.hwaddr = mac
-          clients.useragent = req.headers['user-agent']
-          clients.created_at = new Date()
-          delete clients.iptabled
+				var r_ip = req.headers['x-real-ip'] || req.socket.remoteAddress;
+        if (doc.clients[i].ipv4 == r_ip) {
+console.log('UPDATE-IPUPDATE');
+					doc.clients[i] = client;
+					found = true;
           break;
         }
       }
+			if (!found) { 
+console.log('UPDATE-ADDCLIENT');
+				doc.clients.push(client);
+			}
     }
 //console.log(JSON.stringify(doc))
     streamie.save(doc, function(err, doc) {
@@ -113,48 +120,71 @@ streamie.request('PUT', '/streamie', function(err, result) {
 	  res.end('Weg hier \n');
 	}).listen(8124, "127.0.0.1");
 
-  var updateIPTables = function(id) {
+	var iptables = function(para, fn) {
+		var iptables  = require('child_process').spawn('sudo', ['/sbin/iptables'].concat(para))
+		iptables.on('exit', function(code) {
+			console.log('iptables:'+para.join(' ')+"=>"+code);
+			fn(code);
+		});
+	}
+	
+  var updateIPTables = function(id, rev) {
     streamie.get(id, function(err, doc) {
       if (err) { console.error('couchdb:get:failure:'+err) }
-      if (doc.completed) { return }
+      if (doc._rev != rev) { return }
+      if (doc.completed && doc.completed.pid == process.pid) { return }
       for(var i = doc.clients.length-1; i >= 0; --i) {
         var client = doc.clients[i]
-        if (!client.iptabled) { 
-          console.log('iptables -D INPUT -i eth0 -m mac --mac-source '+client.hwaddr+' -s '+client.ipv4+' -j ACCEPT')
-          console.log('iptables -A INPUT -i eth0 -m mac --mac-source '+client.hwaddr+' -s '+client.ipv4+' -j ACCEPT')
-          client.iptabled = new Date()
-        }
+
+				// $IPTABLES -t mangle -I FREE_MACS -i $CONF_IF -p all -m mac 
+				// --mac-source c8:bc:c8:4f:d4:66 -s 10.205.0.100 -j MARK --set-mark 0x1205
+				var iptable = [];
+				iptable.push('-t');
+				iptable.push('mangle');
+				iptable.push('-I');
+				iptable.push('FREE_MACS');
+				iptable.push('-p');
+				iptable.push('all');
+				iptable.push('-m');
+				iptable.push('mac');
+				iptable.push('--mac-source');
+				iptable.push(client.hwaddr);
+				iptable.push('-s');
+				iptable.push(client.ipv4);
+				iptable.push('-j');
+				iptable.push('MARK');
+				iptable.push('--set-mark');
+				iptable.push('0x1205');
+				iptables(iptable, function(code) {
+					client.iptabled = { date: new Date(), exitcode: code, rev: doc._rev }
+				});
       }
-      doc.completed = new Date()
+      doc.completed = { date: new Date(), pid: process.pid, rev: doc._rev };
       streamie.save(doc, function(err, doc) {
         if (err) {
           console.log('updateIPTables failed:'+err)
-          updateIPTables(id)
+          updateIPTables(id, rev)
         } 
       })
     })
   }
-
-//  var _streamie = CouchClient('http://localhost:5984/streamie')
-  fs.readFile('streamie.json', 'utf8', function(err, data) {
-    since = data || '0'
-    if (err) { since = '0' } 
-    console.log('CHANGES:'+since)
-    streamie.changes(since, function(err, changes) {
-      if (err) {
-        console.error('couchdb:changes:failure:'+err)
-        return
-      }
-      fs.writeFile('streamie.json', changes.seq+'', 'utf8', function(err) {
-        if (err) {
-          console.error('couchdb:write:failure:'+err)
-          return
-        }
-        if (changes.deleted) { return }
-            updateIPTables(changes.id)
-      })
-    })
-  })
+	iptables(['-t', 'mangle', '-F', 'FREE_MACS'], function(code) {
+		iptables(['-t', 'mangle', '-A', 'FREE_MACS', '-j', 'RETURN'], function(code) {
+			streamie.changes(0, function(err, changes) {
+					if (err) {
+						console.error('couchdb:changes:failure:'+err)
+						return
+					}
+					if (changes.deleted) { 
+console.log('DELETE:'+util.inspect(changes));
+						return 
+					}
+					for(var i in changes.changes) {
+						updateIPTables(changes.id, changes.changes[i].rev )
+					}
+			})
+		})
+	})
 })
 
 console.log('Server running at http://127.0.0.1:8124/');
--- flow/flow2couchdb.js
+++ flow/flow2couchdb.js
@@ -1,10 +1,15 @@
 
 var fs = require('fs');
+var util = require('util');
 var http = require('http');
 var cp = require('child_process');
+var CouchClient = require('./couch-client');
+var _ = require('underscore')._;
+
+require('datejs');
 
 var Server = { host: '172.16.143.8', port: 9200, path: '/traffic', headers: { 'content-type': 'application/json' } };
-var Server = { host: '172.16.143.8', port: 5984, path: '/traffic', headers: { 'content-type': 'application/json' } };
+var Server = { host: '127.0.0.1', port: 5984, path: '/traffic', headers: { 'content-type': 'application/json' } };
 
 //flow-cat -t '3/14/11 9:31:00'  -T '3/14/11 9:32:00' -z9 2011 | flow-print
 var formatDate = function(date) {
@@ -31,115 +36,226 @@ var setStartDate = function(date, done) {
 
 var gid = 1;
 var running = 0;
-var storeCouch = function(cmds, id, cmd, i, found, count) {
-	found = false	
+var storeCouch = function(cmds, complete, id, cmd, i, found, count) {
+console.log('ENTER-storeCouch'+util.inspect(cmds));
+	found = false;
 	for (i = running; i < 12; ++i) {
 		running = 12;
 		found = true;
 //console.log('QUEUE:'+i);
-		storeCouch(cmds, id, cmd);
+		storeCouch(cmds, complete, id, cmd);
 	}
 	if (found) {
 		return
 	}
-	
 	cmd = cmds.shift();
 	if (!cmd) {
 		return;
 	}
+console.log('START-storeCouch'+util.inspect(cmd));
 	//srcIP            dstIP            prot  srcPort  dstPort  octets      packets
 	id = ++gid;
 	count = 0;
 	var run = function() {
 		cp.exec(cmd.cmd.join(' '), { maxBuffer: 10*1024*1024 }, function(error, stdout, stderr, req, lines, out) {
-console.log('WROTE:'+cmd.end);
+//console.log('WROTE:'+cmd.key);
 			if (error) {
 console.log('ERROR:'+id+":"+error+":"+cmd.cmd.join(' '));
 				if (count++ < 3) {
 					return run();
 				}
-				setStartDate(cmd.end, function() {
-					running--;
-					storeCouch(cmds)
-				})
+				running--;
+				storeCouch(cmds, complete)
 				return;
 			}
 			lines = stdout.split("\n");
+//console.log('LINES:'+util.inspect(lines));
 			delete stdout;
 			delete stderr;
 			lines.shift();
 			out = [];
 			var out = {
-				     _id: ""+cmd.start.getTime(),
-				     start: cmd.start,
-				     end: cmd.end,
-				     tuples: []
+				     _id: cmd.key,
+				     tuples: [],
+						 created_at: Date()
 				  };
 			for(var line in lines) {
 				line = lines[line];
 				var cols = line.split(/[ ]+/)
-				out.tuples.push({
-					srcIP: cols[0],
-					dstIP: cols[1],
-					prot: cols[2],
-					srcPort: cols[3],
-					dstPort: cols[4],
-					octets: cols[5],
-					packets: cols[6]
-				      });
+				if (cols.length >= 7) { 
+					out.tuples.push({
+						srcIP: ip2twitter[cols[0]] || cols[0],
+						dstIP: ip2twitter[cols[1]] || cols[1],
+						prot: cols[2],
+						srcPort: cols[3],
+						dstPort: cols[4],
+						octets: cols[5],
+						packets: cols[6]
+								});
+				}
 			}
-			Server.method = 'POST';
-			req = http.request(Server, function(res) {
-	/*
-				  res.on('data', function (chunk) {
-				    console.log('BODY: ' + chunk);
-				  });
-	*/
-	//console.log(res);
+			if (!out.tuples.length) { 
 				delete lines;
 				delete cols;
 				delete data;
 				delete req;
-				setStartDate(cmd.end, function() {
-					running--;
-					storeCouch(cmds)
-				})
+				running--;
+				storeCouch(cmds, complete)
+				if (!cmds.length) { console.log('storeCouch-COMPLETE'); complete(); }
+				return; 
+			}
+//console.log('OUT:'+util.inspect(out));
+      traffic.save(out, function(err, doc) {
+				delete lines;
+				delete cols;
+				delete data;
+				delete req;
+				running--;
+				storeCouch(cmds, complete)
+				if (!cmds.length) { console.log('storeCouch-COMPLETE'); complete(); }
 			})
-			req.end(JSON.stringify(out));
 		})
 	}
 	run();
 }
 
-Server.method = 'PUT';
-var req = http.request(Server, function(res) {
+var ip2twitter = {};
 
-getStartDate(function(startDate) {
-	if (!startDate) {
-		startDate = new Date((~~((new Date()).getTime()/86400000)*86400000)-(2*86400000));
-	} 
-	endDate = new Date(~~((new Date()).getTime()/60000)*60000);
-console.log(startDate+"=>"+endDate)
-	var cmds = []
-	for (var current = startDate.getTime(); current < endDate.getTime(); current += 60000) {
-		var start = new Date(current)
-		var end = new Date(current+60000)
-		var cmd = ['flow-cat'];
-		cmd.push('-t')				
-		cmd.push(formatDate(start));
-		cmd.push('-T')				
-		cmd.push(formatDate(end))
-		cmd.push('/tmp/2011')
-		cmd.push('| flow-print')
-		cmds.push({ cmd: cmd, start: start, end: end})
+var lastStreamieChange = null;
+var streamie = new CouchClient('http://localhost:5984/streamie');
+streamie.changes(0, function(err, changes) {
+	if (err) {
+		console.error('couchdb:changes:failure:'+err)
+		return
 	}
-	storeCouch(cmds);
-/*
-	setStartDate(endDate, function() {
-				
+	if (changes.deleted) { 
+		return 
+	}
+	streamie.get(changes.id, function(err, doc) {
+		if (err) {
+			console.error('couchdb:changes:failure:'+err)
+			return
+		}
+		for (var i in doc.clients) { 
+			var client = doc.clients[i];
+console.log('ADD IP2Twitter:'+client.ipv4+"=>"+doc.twitter.screen_name);
+			ip2twitter[client.ipv4] = doc.twitter.screen_name;
+		} 
+		lastStreamieChange = new Date();
 	})
-*/
-
 })
 
-}).end();
+crawler = function(base, completed, data) {
+	data = data || { dirs: [], files: [], calls: 0 };
+	data.calls++;
+console.log('IN-CRAWLER:'+base+":"+data.calls);
+
+	fs.readdir(base, function(err, in_files, dirs, cnt, i) {
+		if (err)  {
+			console.log('fs.readdir:err:'+err);
+		  return;
+		}
+		dirs = [];
+		cnt = 0;
+		for(i in in_files) {
+			(function(fname, dir) {
+				fs.stat(base+'/'+fname, function(err, stat) {
+					if (err)  {
+						console.log('fs.readdir:err:'+err);
+						return;
+					}
+					if (stat.isFile()) { data.files.push(base+'/'+fname); } 
+					else if (stat.isDirectory()) { dirs.push(base+'/'+fname); }
+					if (++cnt == in_files.length) {
+						data.dirs.push.apply(data.dirs, dirs);
+						for(dir in dirs) {
+							crawler(dirs[dir], completed, data);
+						}
+						data.calls--;
+console.log('OUT-CRAWLER-A:'+base+":"+data.calls);
+//console.log('DATA:'+util.inspect(data));
+						if (!data.calls) {
+							completed(data);
+						}
+					}
+				})
+			})(in_files[i]);
+		}
+		if (!in_files.length) {
+			data.calls--;
+console.log('OUT-CRAWLER-B:'+base+":"+data.calls);
+			if (!data.calls) {
+				completed(data);
+			}
+		}
+  })	
+}
+
+var traffic = new CouchClient('http://localhost:5984/traffic');
+var observedDirs = {};
+setTimeout(function CheckChanges() { 
+	var now = new Date();
+	if (lastStreamieChange && (now.getTime()-lastStreamieChange.getTime()) > 1000) {
+		console.log('START-CRAWLER'+util.inspect(ip2twitter));
+		traffic.request('PUT', '/traffic', function(err, result) {
+		console.log('AAAA');
+			if (err) {
+				console.error('couchdb:traffic:failure:'+err)
+				return
+			}
+			var running = false;
+			var crawlerQueue = [];
+			crawler('/flows', function AddFlows(data) {
+				for(var i in data.dirs) {
+					(function CheckDir(dir) { 
+						if (observedDirs[dir]) { return; };	
+						observedDirs[dir] = true;
+console.log('OBSERVER:'+dir);
+
+						fs.watchFile(dir, function() {
+console.log('dir='+dir);
+							if (running) { 
+								crawlerQueue.push(dir);
+								return;
+							} 
+							running = true;
+							crawler(dir, AddFlows);
+						})
+
+					})(data.dirs[i]);
+				}
+				//var re = new RegExp("^.*\/ft-v05.\(\d+\-\d+\-\d+.\d+\)\+0200");
+				var re = new RegExp("^.*\/ft-v05\.\(\\d+-\\d+-\\d+\.\\d+\)\\+\\d+$");
+				var cmds = [];
+				for(var i in data.files) {
+					var file = data.files[i];
+					var res = re.exec(file);
+					if (res) {
+console.log('ADD-Flow:'+file);
+						cmds.push({key: res[1], cmd: ['flow-print', '<', file, '&&', 'mv', file, file+'-done']})
+						//cmds.push({key: res[1], cmd: ['flow-print', '<', file]});
+					}
+				}
+				if (!cmds.length) {
+					running = false;
+					var dir = crawlerQueue.shift();
+					if (dir) { crawler(dir, AddFlows); }
+				}
+				storeCouch(cmds, function() {
+					running = false;
+					var dir = crawlerQueue.shift();
+					if (dir) { crawler(dir, AddFlows); }
+				});
+			});
+		})
+	} else {
+		setTimeout(CheckChanges, 1000);
+	}
+}, 1000);
+
+
+
+
+//console.log("MENO"+Date.parseExact("2011-04-10", "yyyy-M-d"))
+//console.log("MENO"+Date.parseExact("2011-04-10.174750", "yyyy-M-d.HHmmss"))
+
